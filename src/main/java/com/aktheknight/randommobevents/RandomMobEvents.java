6a90cfce93d449ac074345e005039edf2eae6733
AKTheKnight <AKTheKnight.minecraft@gmail.com>
2016-01-22 10:38:15 +0000

    The real 1.0.1 update. Fixed issues. Working now

--- src/main/java/com/aktheknight/randommobevents/RandomMobEvents.java
+++ src/main/java/com/aktheknight/randommobevents/RandomMobEvents.java
@@ -15,8 +15,10 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class RandomMobEvents {
 	public static final String MODID = "RandomMobEvents";
 	public static final String MODNAME = "RandomMobEvents";
-	public static final String VERSION = "1.8.9-1.0.0";
+	public static final String VERSION = "1.8.9-1.0.1";
 	
+	@Mod.Instance
+	public static RandomMobEvents instance;
 	
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	
