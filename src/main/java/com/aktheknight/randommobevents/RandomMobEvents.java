37cf4eaf5e50f12085c9a5430c28b24bddfb8030
AKTheKnight <AKTheKnight.minecraft@gmail.com>
2016-02-01 21:43:26 +0000

    Mod not required client side

--- src/main/java/com/aktheknight/randommobevents/RandomMobEvents.java
+++ src/main/java/com/aktheknight/randommobevents/RandomMobEvents.java
@@ -11,12 +11,12 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid=RandomMobEvents.MODID, name=RandomMobEvents.MODNAME, version=RandomMobEvents.VERSION)
+@Mod(modid=RandomMobEvents.MODID, name=RandomMobEvents.MODNAME, version=RandomMobEvents.VERSION, acceptableRemoteVersions = "*")
 
 public class RandomMobEvents {
 	public static final String MODID = "RandomMobEvents";
 	public static final String MODNAME = "RandomMobEvents";
-	public static final String VERSION = "1.8.9-1.0.4";
+	public static final String VERSION = "1.8.9-1.0.5";
 	
 	@Mod.Instance
 	public static RandomMobEvents instance;
